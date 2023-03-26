01f58d5b4391ea05dbf057cbc16e58ab860a3757
Oksana <oksana.sydorenko0691@icloud.com>
2023-03-26 17:40:04 +0300

    hw11

--- src/api-service.js
+++ src/api-service.js
@@ -11,7 +11,7 @@ export default class NewsApiService {
       method: 'get',
       url: 'https://pixabay.com/api/',
       params: {
-        key: '34523545-f21683fd59bfc3e4e2549fe07',
+        key: '34751738-d2b2bc2823b3d8a283ca4d922',
         q: `${this.searchQuery}`,
         image_type: 'photo',
         orientation: 'horizontal',
