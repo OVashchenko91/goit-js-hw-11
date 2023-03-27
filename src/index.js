3d091ced1c2402fd5343ffb941aa7574d957a3d7
Oksana <oksana.sydorenko0691@icloud.com>
2023-03-27 20:50:06 +0300

    hw11

--- src/index.js
+++ src/index.js
@@ -58,8 +58,11 @@ function onLoadMore() {
   fetchGallery();
 }
 
-// async function fetchGallery() {
-  refs.loadMoreBtn.classList.add('is-hidden');//
+
+
+
+async function fetchGallery() {
+  refs.loadMoreBtn.classList.add('is-hidden');
 
   const r = await newsApiService.fetchGallery();
   const { hits, total } = r;
@@ -73,7 +76,7 @@ function onLoadMore() {
     return;
   }
 
-    onRenderGallery( hits );
+    onRenderGallery(hits);
     
   isShown += hits.length;
 
@@ -85,50 +88,7 @@ function onLoadMore() {
   if (isShown >= total) {
     Notify.info("We're sorry, but you've reached the end of search results.");
   }
-
-
-function onRenderGallery(elements) {
-  const markup = elements
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<div class="photo-card">
-    <a href="${largeImageURL}">
-      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-    </a>
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-        ${likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-        ${views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-        ${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-        ${downloads}
-      </p>
-    </div>
-    </div>`;
-      }
-    )
-    .join('');
-  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
-  lightbox.refresh();
 }
-//
 
 async function fetchGallery() {
   refs.loadMoreBtn.classList.add('is-hidden');
